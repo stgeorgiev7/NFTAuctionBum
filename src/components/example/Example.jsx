0472123b18d6892e8b1ac7a89145933758563a8c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-14 18:30:09 +0300

    added logo

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,12 +3,15 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import Logo from '../logo/Logo'
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
+          <Logo />
+          <Logo type='muted' />
           <Link href="/about" color="secondary">
             Hi
           </Link>
